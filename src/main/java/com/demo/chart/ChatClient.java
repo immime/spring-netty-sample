5ad1031c085735aa49be331ff3949c55e73d917f
John Wen <johnwenblabla@gmail.com>
2016-09-05 18:07:00 +0800

    Chat Demo

---
+++ src/main/java/com/demo/chart/ChatClient.java
@@ -0,0 +1,45 @@
+package com.demo.chart;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class ChatClient {
+	public static void main(String[] args) throws Exception{
+        new ChatClient("localhost", 8080).run();
+    }
+
+    private final String host;
+    private final int port;
+
+    public ChatClient(String host, int port){
+        this.host = host;
+        this.port = port;
+    }
+
+    public void run() throws Exception{
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap  = new Bootstrap()
+                    .group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChatClientInitializer());
+            Channel channel = bootstrap.connect(host, port).sync().channel();
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            while(true){
+                channel.writeAndFlush(in.readLine() + "\r\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
+
+    }
+}
+
--- src/main/java/com/demo/chart/ChatClientHandler.java
+++ src/main/java/com/demo/chart/ChatClientHandler.java
@@ -5,7 +5,7 @@ import java.util.Date;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
-public class ClientHandler extends SimpleChannelInboundHandler<String> {
+public class ChatClientHandler extends SimpleChannelInboundHandler<String> {
 
 	@Override
 	protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
